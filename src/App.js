9b389c47c956d4cd6d96f59b7edaff938304b6ac
Shaun Evans <shaun.evans@high-view-studios.co.uk>
2020-06-11 10:45:53 +0100

    Navigation Bar tested with simple signin and administration pages built as components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./public/styles.css";
+
+import Nav from "./components/Nav";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Adminitration from "./pages/Administration";
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App" >
+        <Nav />
+        <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/administration" component={Adminitration} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Administration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Administration() {
+  return (
+    <div >
+      <h1>Administration Page</h1>
+    </div>
+  );
+}
+
+export default Administration;
