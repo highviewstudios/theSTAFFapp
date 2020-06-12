40925a2596ac94156daa4bcde6a8d6422556019c
Shaun Evans <shaun.evans@high-view-studios.co.uk>
2020-06-12 13:01:38 +0100

    Add build folder and server.js file, edited .gitignore

---
+++ build/precache-manifest.6434fe1e7b0d5a30d9b7e80b717dc9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5dbda0f12634ec53b157a74f41a9bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b48ef1aa844730c20cf",
+    "url": "/static/css/main.fb5cce60.chunk.css"
+  },
+  {
+    "revision": "c8e8875ebdca686d1444",
+    "url": "/static/js/2.ce78fc1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ce78fc1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b48ef1aa844730c20cf",
+    "url": "/static/js/main.170eb351.chunk.js"
+  },
+  {
+    "revision": "b92c52f154cac2572a99",
+    "url": "/static/js/runtime-main.2d81340a.js"
+  }
+]);
