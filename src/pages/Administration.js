280525dbc41b72b47a6e5ec075e7f83a84d4d831
Shaun Evans <shaun.evans@high-view-studios.co.uk>
2020-06-26 12:12:57 +0100

    Add redirect function

--- src/pages/Administration.js
+++ src/pages/Administration.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {usestate, useEffect} from 'react';
 
 function Administration() {
+
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+  useEffect(() => {
+    getUserAsync()
+  },[])
+
+  async function getUserAsync() {
+  // let response = await fetch(url, {
+  //   method:'GET',
+  //   mode:'no-cors', 
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //     'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, HEAD, OPTIONS'}})
+  
+  // let data = await response.json({
+  //   data: response
+  // });
+  // console.log(data);
+}
+
   return (
     <div >
       <h1>Administration Page</h1>
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {usestate, useEffect, useState} from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
 
 function SignIn() {
+
+  const history = useHistory();
+
+  
+  
   return (
     <div >
       <h1>Sign In Page</h1>
+
+      <button onClick={Run}>Hello</button>
     </div>
   );
+
+  function Run() {
+    alert("HELLO");
+    history.push("/administration");
+  }
 }
 
 export default SignIn;
